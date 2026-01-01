916138d63dc52bc95e63b36ff42174214bd9b778
Abhay Tiwari <tiwari.abhay7978@gmail.com>
2026-01-01 13:02:22 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-center">
+      <h1 className="text-center, cursor-pointer">hello nextjs
+
+      </h1>
+      </div>
+  )
+  }
